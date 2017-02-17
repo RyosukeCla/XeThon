5975dca7ed613e6b6520abae2830ca02056a8df9
RyosukeCla <ryosukeclarinet@gmail.com>
2017-02-17 10:48:21 +0900

    fixed react template

--- bin/react/template/react/component.jsx
+++ bin/react/template/react/component.jsx
@@ -2,7 +2,7 @@ import React, {PropTypes, Component} from 'react';
 
 import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux';
-//import * as Actions from './S_NAMEActions';
+//import * as Actions from './S_NAMEAction';
 
 class C_NAME extends Component {
   render() {
