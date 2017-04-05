dfeb3c48c5dac8fa109f3d0f31a428ec8d7317b4
RyosukeCla <ryosukeclarinet@gmail.com>
2017-04-05 15:00:38 +0900

    add alot and fix xethon mode

--- app/backend/hello.py
+++ app/backend/hello.py
@@ -1,6 +1,44 @@
 import main.maker.project as pr
+import main.compiler.xethon_compiler as xc
+import os
 
-test = pr.Project('./test')
-test.make()
+tn = xc.Tokenizer([','])
+res = tn.tokenize('Hi, I am Hello!')
+
+print(res)
+#test = pr.Project('./test')
+#test.make()
 
 print("Hi, I am Hello!")
+
+test_str = R"""
+<<<
+def test():
+    return 'f(x) = x^2'
+
+def nest(x):
+    return r'\begin{}' + x + '\end{}'
+fx = test()
+>>>
+hello world. <<fx>>
+<<<
+ts = nest(fx)
+>>>
+
+hello world. <<ts>>
+"""
+
+tn = xc.Tokenizer(['(\\\\<|\\\\>)', '(<|>)'])
+res = tn.tokenize(test_str)
+
+print(res)
+
+xtoke = xc.XethonTokenizer()
+res = xtoke.tokenize(test_str)
+print(res)
+
+xcom = xc.XethonCompiler()
+res = xcom.hage(res)
+print(res)
+
+exec(res)
---
+++ app/backend/test/regex.test.py
@@ -0,0 +1,19 @@
+import re
+import StringIO
+
+
+test_str = """
+hello world\<\<<< today >>\> aiueo << a >>
+
+<<<
+def aiueo():
+  pass
+>>>
+
+aeiuo
+"""
+
+test_2 = """
+def py():
+    pass
+"""
